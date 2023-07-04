657ab3f77375042b24629443fa4faa940b4997b5
Amaan Qureshi <amaanq12@gmail.com>
2023-07-04 18:23:05 -0400

    chore: update manifests, add ci & test scripts

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -31,7 +31,7 @@ pub fn language() -> Language {
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
 /// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
-pub const NODE_TYPES: &'static str = include_str!("../../src/node-types.json");
+pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
 // Uncomment these to include any queries that this grammar contains
 
@@ -47,6 +47,6 @@ mod tests {
         let mut parser = tree_sitter::Parser::new();
         parser
             .set_language(super::language())
-            .expect("Error loading astro language");
+            .expect("Error loading astro grammar");
     }
 }
