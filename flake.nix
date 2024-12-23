0ad33e32ae9726e151d16ca20ba3e507ff65e01f
Ruby Iris Juric <ruby@srxl.me>
2024-12-23 18:38:10 +1100

    feat: provide nixpkgs overlay in flake

--- flake.nix
+++ flake.nix
@@ -5,18 +5,29 @@
         flake-utils.url = "github:numtide/flake-utils";
     };
 
-    outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: let
+    outputs = { self, nixpkgs, flake-utils }: (flake-utils.lib.eachDefaultSystem (system: let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        appliedOverlay = self.overlays.default pkgs pkgs;
     in {
         devShell = pkgs.mkShell {
             buildInputs = with pkgs; [
                 tree-sitter
             ];
         };
-        packages.default = pkgs.tree-sitter.buildGrammar {
-            language = "astro";
-            src = pkgs.lib.cleanSource ./.;
-            version = self.shortRev or "latest";
+
+        packages.default = appliedOverlay.tree-sitter.passthru.builtGrammars.tree-sitter-astro;
+    })) // {
+        overlays.default = final: prev: {
+            tree-sitter = prev.tree-sitter.override {
+                extraGrammars = {
+                    tree-sitter-astro = prev.tree-sitter.buildGrammar {
+                        language = "astro";
+                        src = prev.lib.cleanSource ./.;
+                        version = self.shortRev or "latest";
+                    };
+                };
+            };
         };
-    });
+    };
 }
